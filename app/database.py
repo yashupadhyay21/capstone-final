f395a0287a1191e0c8014b7c135d45c8bac8873c
Yash Upadhyay <yashupadhyay2109@gmail.com>
2025-04-20 13:22:32 +0530

    Working state with prediction, login, signup, and DB integration completed

--- app/database.py
+++ app/database.py
@@ -1,3 +1,5 @@
+# database.py
+
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -5,8 +7,8 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), default='unknown')
-    email = db.Column(db.String(100), default='unknown@example.com')
-    password = db.Column(db.String(150), nullable=True)  # Temporarily allow null
+    email = db.Column(db.String(100), unique=True, nullable=False)  # Ensure email is unique and required
+    password = db.Column(db.String(150), nullable=False)  # Password cannot be null
     prediction_score = db.Column(db.Float, nullable=False)
     prediction_result = db.Column(db.String(100), nullable=False)
 
